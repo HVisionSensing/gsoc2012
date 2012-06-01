8d5eb210fc063ab109c5532fb2c7a6e70b5bd5df
Duffy Cola <duffycola@gmail.com>
2012-06-01 22:40:20 +0200

    IntroCamera: initial view-based project

---
+++ IntroCamera/IntroCamera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IntroCamera
+//
+//  Created by Eduard Feicho on 01.06.12.
+//  Copyright (c) 2012 Eduard Feicho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
